556c2100973aab35bfc34d24805cef54f9be07c0
Alexandre Stachnik <astachni@z1r5p4.42lyon.fr>
2022-11-22 23:49:38 +0100

    a

--- test.c
+++ test.c
@@ -6,7 +6,7 @@ int	main(void)
 	char	c = 'k';
 	char	d = 'x';
 	void	*p = malloc(9);
-	printf("vrai fonction %c\n", c);
-	ft_printf("ma fonction %c\n", c);
+	printf ("%d", printf("vrai fonction %c\n"));
+	ft_printf("ma fonction %c\n", d);
 	free(p);
 }
